75150786ef88f855dff3551da4b3bcc7bb24b8e0
Jared Womack <womackjw@hotmail.com>
2021-08-31 08:15:30 -0700

    added META to lessons index and [id] pulling meta for [id] from frontmatter on backend posts

--- pages/lessons/[id].jsx
+++ pages/lessons/[id].jsx
@@ -39,46 +39,49 @@ export const getStaticProps = async (context) => {
 
 export default function Lesson({ frontmatter, content }) {
   return (
-    <div className="flex  w-full justify-center min-h-screen">
-      <main className="min-h-screen w-full">
-        <div className="post px-10  ">
-          <div className="w-full ">
-            <div className="relative overflow-hidden pb-2/3 rounded my-24">
-              <h1 className="lg:text-7xl text-3xl text-center mt-4 mb-24 leading-normal ">
-                {frontmatter.title}
-              </h1>
+    <>
+      <Meta title={frontmatter.metaTitle} desc={frontmatter.metaDesc} />
+      <div className="flex  w-full justify-center min-h-screen">
+        <main className="min-h-screen w-full">
+          <div className="post px-10  ">
+            <div className="w-full ">
+              <div className="relative overflow-hidden pb-2/3 rounded my-24">
+                <h1 className="lg:text-7xl text-3xl text-center mt-4 mb-24 leading-normal ">
+                  {frontmatter.title}
+                </h1>
 
-              <Image
-                width={1691}
-                height={1137}
-                src={frontmatter.imageURL}
-                alt=""
-                className="absolute min-h-0 w-full object-cover"
-              />
-            </div>
-            <p className="by-line font-semibold text-2xl mb-12">
-              Author: {frontmatter.author}
-            </p>
-
-            <div
-              dangerouslySetInnerHTML={{ __html: marked(content) }}
-              className="prose prose-lg  xl:prose-xl max-w-none mb-36"
-            ></div>
-            {/* Render video component if there is an embedID in the frontmatter */}
-            {frontmatter.embedID && (
-              <div className="aspect-w-16 aspect-h-9 my-24">
-                <iframe
-                  className="rounded "
-                  src={`https://www.youtube.com/embed/${frontmatter.embedID}`}
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
+                <Image
+                  width={1691}
+                  height={1137}
+                  src={frontmatter.imageURL}
+                  alt=""
+                  className="absolute min-h-0 w-full object-cover"
+                />
               </div>
-            )}
+              <p className="by-line font-semibold text-2xl mb-12">
+                Author: {frontmatter.author}
+              </p>
+
+              <div
+                dangerouslySetInnerHTML={{ __html: marked(content) }}
+                className="prose prose-lg  xl:prose-xl max-w-none mb-36"
+              ></div>
+              {/* Render video component if there is an embedID in the frontmatter */}
+              {frontmatter.embedID && (
+                <div className="aspect-w-16 aspect-h-9 my-24">
+                  <iframe
+                    className="rounded "
+                    src={`https://www.youtube.com/embed/${frontmatter.embedID}`}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   );
 }
--- pages/lessons/index.jsx
+++ pages/lessons/index.jsx
@@ -5,47 +5,53 @@ const matter = require("gray-matter");
 
 const LessonsIndex = ({ lessons }) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <main className="min-h-screen">
-        <h1 className="text-7xl my-20 text-center">All Lessons</h1>
-        <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-12">
-          {lessons.map((lesson) => (
-            <Link href={"/lessons/" + lesson.id} key={lesson.id} className="">
-              <div className="rounded overflow-hidden shadow-lg cursor-pointer">
-                <div className="text-center">
-                  <Image
-                    width={478}
-                    height={256}
-                    src={lesson.frontmatter.imageURL}
-                    alt=""
-                    className="w-full h-96"
-                  />
-                </div>
-                <div className="px-6 py-4">
-                  <div className="font-bold text-xl mb-2">
-                    {lesson.frontmatter.title}
+    <>
+      <Meta
+        title="Lessons | USA English Tutor has a wide variety of lessons. We offer beginner, intermediate and advanced lessons to help you on your ESL journey and we can help you become fluent in Enlglish quickly."
+        desc="Our lessons on USA English Tutor are free and offer a wide array of topics for ESL learners. We teach ESL learner how to read, write, listen and speak English with a neutral American accent. We have a wide variety of videos on youtube and are always posting new English learning materials on social media. We offer lessons that concentrate on business, college, medical, traveling and everything else that encompasses learning English."
+      />
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <main className="min-h-screen">
+          <h1 className="text-7xl my-20 text-center">All Lessons</h1>
+          <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-12">
+            {lessons.map((lesson) => (
+              <Link href={"/lessons/" + lesson.id} key={lesson.id} className="">
+                <div className="rounded overflow-hidden shadow-lg cursor-pointer">
+                  <div className="text-center">
+                    <Image
+                      width={478}
+                      height={256}
+                      src={lesson.frontmatter.imageURL}
+                      alt=""
+                      className="w-full h-96"
+                    />
                   </div>
-                  <p className="text-gray-700 text-base">
-                    {lesson.frontmatter.excerpt}
-                  </p>
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">
+                      {lesson.frontmatter.title}
+                    </div>
+                    <p className="text-gray-700 text-base">
+                      {lesson.frontmatter.excerpt}
+                    </p>
+                  </div>
+                  <div className="px-6 pt-4 pb-2">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                      {lesson.frontmatter.level}
+                    </span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                      {lesson.frontmatter.contentType}
+                    </span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                      {lesson.frontmatter.simpleLevel}
+                    </span>
+                  </div>
                 </div>
-                <div className="px-6 pt-4 pb-2">
-                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    {lesson.frontmatter.level}
-                  </span>
-                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    {lesson.frontmatter.contentType}
-                  </span>
-                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    {lesson.frontmatter.simpleLevel}
-                  </span>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      </main>
-    </div>
+              </Link>
+            ))}
+          </div>
+        </main>
+      </div>
+    </>
   );
 };
 
