a7f02c279fefd6761094dc93105e03d44038e8d7
Jared Womack <womackjw@hotmail.com>
2021-08-31 09:29:26 -0700

    next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   images: {
-    domains: ['images.unsplash.com']
-  }
+    domains: ["images.unsplash.com"],
+  },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/robots.txt",
+        destination: "/api/robots",
+      },
+    ];
+  },
 };
