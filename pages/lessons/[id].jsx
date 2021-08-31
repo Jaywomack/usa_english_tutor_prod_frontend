938db1e6e7e9b5b3792312d77714129642b25cdb
Jared Womack <womackjw@hotmail.com>
2021-08-31 08:43:01 -0700

    fixed Meta Imports

--- pages/lessons/[id].jsx
+++ pages/lessons/[id].jsx
@@ -2,6 +2,7 @@ const matter = require("gray-matter");
 import marked from "marked";
 import { useState } from "react";
 import Image from "next/image";
+import Meta from "../../components/partials/Meta";
 
 export const getStaticPaths = async () => {
   const res = await fetch(
